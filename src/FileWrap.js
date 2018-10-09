5763393f9dc3a04694475a821cba9a70919c14ad
MinusGix <minusgix@gmail.com>
2018-10-09 16:02:44 -0500

    Added file descriptor to storage

--- src/FileWrap.js
+++ src/FileWrap.js
@@ -3,5 +3,39 @@ const fs = require('fs');
 class FileWrap {
 	constructor (fileDir) {
 		this._fileDir = fileDir;
+		this._loaded = new Buffer();
+		this.fd = null;
+		
+	}
+
+	get loaded () {
+		return this.fd !== null;
+	}
+
+	init () {
+		this._open()
+			.then(fd => this.fd = fd);
+	}
+
+	_open () {
+		return new Promise((resolve, reject) => fs.open(this._fileDir, 'wr', (err, fd) => {
+			if (err) {
+				throw err;
+			}
+
+			resolve(fd);
+		}));
+	}
+
+	_loadData (fd, pos, length) {
+		let buf = new Buffer();
+
+		return new Promise((resolve, reject) => fs.read(fd, buf, 0, length, pos, (err, bytesRead, buffer) => {
+			if (err) {
+				return reject(err);
+			}
+
+			resolve(buffer, bytesRead);
+		}));
 	}
 }
