cfe0ae9c98d3b0f4120951e2eb2824f801102404
MinusGix <minusgix@gmail.com>
2018-10-09 18:22:38 -0500

    Removed requires to bufferwrap

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const bignum = require('bignum');
 const fs = require('fs');
 
-const BufferWrap = require('./src/BufferWrap.js');
 const Err = require('./src/Error.js');
 const FileWrap = require('./src/FileWrap.js');
