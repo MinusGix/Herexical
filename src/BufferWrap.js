0e5cc440c1dee98b3b3676f1a1f642d20c54bf1c
MinusGix <minusgix@gmail.com>
2018-10-09 18:31:11 -0500

    Added read functions to bufferwrap

--- src/BufferWrap.js
+++ src/BufferWrap.js
@@ -1,6 +1,62 @@
 class BufferWrap {
 	constructor () {
-		this._buffer = null;
+		this._buffer = new Buffer();
+	}
+
+	readDoubleBE (offset) {
+		return this._buffer.readDoubleBE(offset);
+	}
+
+	readDoubleLE (offset) {
+		return this._buffer.readDoubleLE(offset);
+	}
+
+	readFloatBE (offset) {
+		return this._buffer.readFloatBE(offset);
+	}
+
+	readFloatLE (offset) {
+		return this._buffer.readFloatLE(offset);
+	}
+
+	readInt8 (offset) {
+		return this._buffer.readInt8(offset);
+	}
+
+	readInt16BE (offset) {
+		return this._buffer.readInt16BE(offset);
+	}
+
+	readInt16LE (offset) {
+		return this._buffer.readInt16LE(offset);
+	}
+
+	readInt32BE (offset) {
+		return this._buffer.readInt32BE(offset);
+	}
+
+	readInt32LE (offset) {
+		return this._buffer.readInt32LE(offset);
+	}
+
+	readUInt8 (offset) {
+		return this._buffer.readUInt8(offset);
+	}
+
+	readUInt16BE (offset) {
+		return this._buffer.readUInt16BE(offset);
+	}
+
+	readUInt16LE (offset) {
+		return this._buffer.readUInt16LE(offset);
+	}
+
+	readUInt32BE (offset) {
+		return this._buffer.readUInt32BE(offset);
+	}
+
+	readUInt32LE (offset) {
+		return this._buffer.readUInt32LE(offset);
 	}
 }
 
