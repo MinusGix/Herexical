d2ba4aedc5d3bc0edb94e043c156ddbc50773385
MinusGix <minusgix@gmail.com>
2018-10-10 16:24:57 -0500

    Added View class for managing the current view of hex

---
+++ src/View.js
@@ -0,0 +1,36 @@
+const FileWrap = require('./FileWrap.js');
+const BufferWrap = require('./BufferWrap.js');
+const Err = require('./Error.js');
+
+class View {
+	constructor (file) {
+		this.fileWrapper = new FileWrap(file);
+
+		this._viewSize = 64; // How many bytes to load whenever its told to load
+		this._position = 0; // the current position in the file by bytes
+
+		this.loaded = false;
+	}
+
+	get viewSize () {
+		return this._viewSize;
+	}
+
+	set viewSize (newViewSize) {
+		this._viewSize = newViewSize;
+		this.loaded = false;
+	}
+
+	get position () {
+		return this._position;
+	}
+
+	set position (newPosition) {
+		this._position = newPosition;
+		this.loaded = false;
+	}
+
+	loadView () {
+		return this.fileWrapper.loadData(this.position, this.viewSize);
+	}
+}
