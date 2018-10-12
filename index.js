d8f77a70ec35a2732cfa16044b7065cc42bea4a1
MinusGix <minusgix@gmail.com>
2018-10-12 17:23:11 -0500

    Added a module.exports to index.js (for being required) and removed a require call to fs

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const bignum = require('bignum');
-const fs = require('fs');
 
 const Err = require('./src/Error.js');
 const FileWrap = require('./src/FileWrap.js');
 const BufferWrap = require('./src/BufferWrap.js');
 const EditStorage = require('./src/EditStorage.js');
 const View = require('./src/View.js');
+
+module.exports = {
+	bignum,
+	Err,
+	FileWrap,
+	BufferWrap,
+	EditStorage,
+	View,
+};
