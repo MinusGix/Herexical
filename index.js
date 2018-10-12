7edb9742eb34a16b8000ae39b9c707c518e482bb
MinusGix <minusgix@gmail.com>
2018-10-12 17:21:10 -0500

    Added requires in index to other parts

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const fs = require('fs');
 const Err = require('./src/Error.js');
 const FileWrap = require('./src/FileWrap.js');
 const BufferWrap = require('./src/BufferWrap.js');
+const EditStorage = require('./src/EditStorage.js');
+const View = require('./src/View.js');
