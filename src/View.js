ccc05e8df06487c47c860fe311c9b9b96f65aee2
MinusGix <minusgix@gmail.com>
2018-10-13 14:59:22 -0500

    Created View#getDataOnOffset to get data near an offset, useful for hex editors

--- src/View.js
+++ src/View.js
@@ -72,6 +72,120 @@ class View extends EventEmitter {
 
 		return true;
 	}
+
+	// Get some data about some stuff at the offset, such as it's values in various types, etc
+	async getDataOnOffset (offset, area=null) {
+		Log.timeStart('getDataOnOffset');
+
+		if (area === null) {
+			area = await this.fileWrapper._loadData(offset, 8); // 8, because the max length of int64 is 8 and that's the highest
+		}
+
+		const endian = this.fileWrapper._loaded.defaultEndian;
+
+		const data = {
+			offset,
+			
+			Int8: await this._getInt8(0, area), // -128 to 127 (256 values, including 0)
+			UInt8: await this._getUInt8(0, area), // 0 to 255 (256 values)
+			
+			Int16: await this._getInt16(0, endian, area), // -32,768 to 32,767 (65,536 values, including 0)
+			UInt16: await this._getUInt16(0, endian, area), // 0 to 65,535 (65,536 values)
+
+			Int32: await this._getInt32(0, endian, area),
+			UInt32: await this._getUInt32(0, endian, area),
+
+			Int64: null, // Needs Int64 support
+			UInt64: null,
+
+			Float32: null, // There is a buffer readFloat(BE|LE) function but I want to make sure before using it
+			Float64: null, // Needs float64 support
+
+			Decimal: null, // Unsure, should this be the entire area size or something like read the bytes in the area by themselves (bless seems to do something like this? Then it's just int64 (assuming this isn't given an area larger than it)
+			Octal: null, // Have to look up how to convert numbers to octal, and decide on the same as Decimal.
+			Binary: null, // Same as above.
+		};
+
+		Log.timeEnd('getDataOnOffset', offset);
+
+		return data;
+	}
+
+	// Get signed Int8. Size: 1 byte. No need for endian.
+	async _getInt8 (offset, area=null) {
+		if (area === null) { // if area is null, then we assume that offset is offset into file rather than offset into area
+			area = await this.fileWrapper._loadData(offset, 1);
+			offset = 0;
+		}
+
+		return area.readInt8(offset);
+	}
+
+	// Get unsigned Int8. Size: 1 byte. No need for endian.
+	async _getUInt8 (offset, area=null) {
+		if (area === null) {
+			area = await this.fileWrapper._loadData(offset, 1);
+			offset = 0;
+		}
+
+		return area.readUInt8(offset);
+	}
+
+	// Get signed Int16. Size: 2 bytes
+	async _getInt16 (offset, endian=0, area=null) {
+		if (area === null) {
+			area = await this.fileWrapper._loadData(offset, 2);
+			offset = 0;
+		}
+
+		if (endian === 0) { // Big Endian
+			return area.readInt16BE(offset);
+		} else { // Little Endian
+			return area.readInt16LE(offset);
+		}
+	}
+
+	// Get unsigned Int16. Size: 2 bytes
+	async _getUInt16 (offset, endian=0, area=null) {
+		if (area === null) {
+			area = await this.fileWrapper._loadData(offset, 2);
+			offset = 0;
+		}
+
+		if (endian === 0) {
+			return area.readUInt16BE(offset);
+		} else {
+			return area.readUInt16LE(offset);
+		}
+	}
+
+	// Get signed Int32. Size: 4 bytes
+	async _getInt32 (offset, endian=0, area=null) {
+		if (area === null) {
+			area = await this.fileWrapper._loadData(offset, 4);
+			offset = 0;
+		}
+
+		if (endian === 0) {
+			return area.readInt32BE(offset);
+		} else {
+			return area.readInt32LE(offset);
+		}
+	}
+
+	// Get unsigned Int32. Size: 4 bytes
+	async _getUInt32 (offset, endian=0, area=null) {
+		if (area === null) {
+			area = await this.fileWrapper._loadData(offset, 4);
+			offset = 0;
+		}
+
+		if (endian === 0) {
+			return area.readUInt32BE(offset);
+		} else {
+			return area.readUInt32LE(offset);
+		}
+	}
 }
 
 module.exports = View;
