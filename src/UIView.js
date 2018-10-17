dfe55c090d7dcf30303bc7b93b6faafbc4b97131
MinusGix <minusgix@gmail.com>
2018-10-16 19:18:07 -0500

    Added tag system to UIView

--- src/UIView.js
+++ src/UIView.js
@@ -3,6 +3,8 @@ const View = require('./View.js');
 class UIView extends View {
 	constructor (file) {
 		super(file);
+
+		this.tags = new TagManager(this);
 	}
 
 	async getLine (offset, byteCount, includeString=false) {
@@ -53,6 +55,52 @@ class UIView extends View {
 			byte >= 161 && // upside down exclamation mark
 			byte <= 255 // y with dots above it
 		)
+	}
+}
+
+class TagManager {
+	constructor (parent) {
+		this.view = parent;
+
+		this.tags = [];
+	}
+
+	// offsetStart: The position of the start of the tag
+	// offsetEnd: The position of the end of the tag
+	// name: the name for displaying (though, it doesn't have to be used as that, or even used at all)
+	// data: any sort of data the program wants to associate with the tag
+	addTag (offsetStart, offsetEnd, name, data) {
+		this.tags.push({offsetStart, offsetEnd, name, data});
+	}
+
+	// item: some entry in the tags array
+	removeTag (item) {
+		return this.removeTagByIndex(this.tags.indexOf(item));
+	}
+
+	removeTagByIndex (index=-1) {
+		if (index === -1) {
+			return false;
+		}
+
+		this.tags.splice(index, 1);
+
+		return true;
+	}
+
+	getTagByIndex (index=-1) {
+		return this.tags[index] || null;
+	}
+
+	getTagsByName (name) {
+		this.tags.filter(tag => tag.name === name);
+	}
+
+	getTagsInRange (rangeStart, rangeEnd) {
+		return this.tags.filter(tag => 
+			(rangeStart >= tag.offsetStart && rangeStart <= tag.offsetEnd) ||
+			(rangeEnd <= tag.offsetEnd && rangeEnd >= tag.offsetStart)
+		);
 	}
 }
 
