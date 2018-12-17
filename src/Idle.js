3bf644b4ae26b826489185e83e01888887743dd3
MinusGix <minusgix@gmail.com>
2018-12-17 03:47:39 -0600

    Added revoke function to idle functions

--- src/Idle.js
+++ src/Idle.js
@@ -1,26 +1,34 @@
 function Idle (retrieveFunc) {
 	let value;
-
-	return () => {
+	let func = () => {
 		// Perhaps should have another boolean which keeps track of if it has been retrieved? Less error prone, but this should be fine.
 		if (value === undefined) {
 			value = retrieveFunc();
 		}
 
 		return value;
-	}
+	};
+	func.revoke = () => {
+		value = undefined;
+	};
+
+	return func;
 }
 
 function AsyncIdle (retrieveFunc) {
 	let value;
-
-	return async () => {
+	let func = async () => {
 		if (value === undefined) {
 			value = await retrieveFunc();
 		}
 
 		return value;
-	}
+	};
+	func.revoke = () => {
+		value = undefined;
+	};
+
+	return func;
 }
 
 module.exports = {
